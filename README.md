70deeeb979607ccc1633ed8650631c58cf660921
Igor Myroshnichenko <imyroshnichenko@gmail.com>
2019-11-29 23:14:48 -0800

    Adds tests

--- README.md
+++ README.md
@@ -1,26 +1,30 @@
 # numericvision
 
-A Python 3 package for detecting numeric (seven-segment) displays in images and running perspective correction on them
+A Python 3 package for detecting
+[numeric (seven-segment) displays](https://en.wikipedia.org/wiki/Seven-segment_display) in images
+and running (perspective correction)[https://en.wikipedia.org/wiki/Perspective_control] on them
 using [OpenCV 4](https://opencv.org).
 
+`detect_box_sequences()` first applies a series of filters to the input image to reduce noise, bring
+out primary contours and bridge gaps between digit segments (see `images.apply_filters()`). Next,
+all resulting contours are analyzed by comparing their geometric properties to a set of criteria
+which define what it means for a contour to resemble a seven-segment digit. Lots of assumptions are
+being made here (see `config.cfg`). A contour resembling a seven-segment digit is used to
+instantiate `Box` objects. Several Boxes lined up in a row make up a box `Sequence`. Sequences are
+contained in a `Bag`.
+
+In other words, raw contours serve as input for an object detection pipeline which produces results
+in the form of a crawlable object tree.
+
 ![Demo](tests/demo.png)
-
-## Dependencies
-
-```
-$ brew install opencv
-```
 
 ## Installation
 
 ```
+$ brew install opencv
+```
+```
 $ pip install numericvision
-```
-
-## Running from the command line
-
-```
-$ python -m numericvision image.jpg
 ```
 
 ## Using the package
@@ -28,5 +32,12 @@ $ python -m numericvision image.jpg
 ```python
 from numericvision import detect_box_sequences
 
-box_sequences = detect_box_sequences('image.jpg')
+box_sequences = detect_box_sequences("image.jpg")
 ```
+
+## Running from the command line
+
+```
+$ python -m numericvision image.jpg
+```
+f
--- tests/test_detect_box_sequences.py
+++ tests/test_detect_box_sequences.py
@@ -1,4 +1,4 @@
-"""Tests for the numericvision.contours module."""
+"""Tests for numericvision.test_detect_box_sequences()."""
 import os.path
 import pytest
 from numericvision import detect_box_sequences
@@ -36,7 +36,7 @@ def test_detect_box_sequences_3():
         (1395, 343) in keys_to_sequences
         and (1460, 545) in keys_to_sequences
         and (1193, 2681) in keys_to_sequences
-        and (1478, 2635) in keys_to_sequences # b, x, b, b
+        and (1478, 2635) in keys_to_sequences  # b, x, b, b
         and (1686, 2603) in keys_to_sequences
     )
 
@@ -48,4 +48,72 @@ def test_detect_box_sequences_4():
         and (2457, 861) in keys_to_sequences
         and (1628, 1828) in keys_to_sequences
         and (2548, 1845) in keys_to_sequences
+    )
+
+
+def test_detect_box_sequences_5():
+    keys_to_sequences = {s.key: s for s in detect_box_sequences(HERE + "/05.jpg")}
+    assert (
+        (2676, 340) in keys_to_sequences
+        and (2745, 663) in keys_to_sequences
+        and (2549, 978) in keys_to_sequences
+        and (577, 1869) in keys_to_sequences
+        and (1685, 1864) in keys_to_sequences
+        and (2826, 1871) in keys_to_sequences
+    )
+
+
+def test_detect_box_sequences_6():
+    keys_to_sequences = {s.key: s for s in detect_box_sequences(HERE + "/06.jpg")}
+    assert (
+        (1547, 579) in keys_to_sequences
+        and (715, 2627) in keys_to_sequences
+        and (1236, 2623) in keys_to_sequences
+    )
+
+
+def test_detect_box_sequences_7():
+    keys_to_sequences = {s.key: s for s in detect_box_sequences(HERE + "/07.jpg")}
+    assert (
+        (1524, 340) in keys_to_sequences
+        and (1556, 566) in keys_to_sequences
+        and (628, 2587) in keys_to_sequences
+        and (1113, 2673) in keys_to_sequences
+        and (1634, 2772) in keys_to_sequences
+    )
+
+
+def test_detect_box_sequences_8():
+    keys_to_sequences = {s.key: s for s in detect_box_sequences(HERE + "/08.jpg")}
+    assert (
+        (1601, 315) in keys_to_sequences
+        and (1623, 551) in keys_to_sequences
+        and (679, 2683) in keys_to_sequences
+    )
+
+
+def test_detect_box_sequences_9():
+    keys_to_sequences = {s.key: s for s in detect_box_sequences(HERE + "/09.jpg")}
+    assert (
+        (2371, 1087) in keys_to_sequences
+        and (2540, 1581) in keys_to_sequences
+    )
+
+
+def test_detect_box_sequences_10():
+    keys_to_sequences = {s.key: s for s in detect_box_sequences(HERE + "/10.jpg")}
+    assert (
+        (1536, 286) in keys_to_sequences
+        and (1605, 550) in keys_to_sequences
+        and (613, 2761) in keys_to_sequences
+        and (1157, 2755) in keys_to_sequences
+        and (1693, 2757) in keys_to_sequences
+    )
+
+
+def test_detect_box_sequences_11():
+    keys_to_sequences = {s.key: s for s in detect_box_sequences(HERE + "/11.jpg")}
+    assert (
+        (551, 616) in keys_to_sequences
+        and (600, 743) in keys_to_sequences
     )
